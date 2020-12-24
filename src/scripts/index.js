1576d15c9ef65c5436e29669f81a0ff0b1207347
mg-kh <tragonn18@gmail.com>
2020-12-25 00:02:08 +0630

    ✨ add overlay

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,24 +5,51 @@ import '../styles/style.scss';
 import {snowFall} from "./snow-fall"
 
 $(document).ready(() => {
-     snowFall()
-     setInterval(() => {
-          snowFall()
-     }, 1000);
-
+     const button = $('.start__button')
+     const overlay = $('.over__lay')
      const heroText = $('.hero__text span')
-     var tl = anime.timeline({
-          easing: 'easeOutExpo',
-          duration: 100,
-     });
-     tl
-     .add({
-          targets: heroText[0],
-          opacity : 1
+
+     button.on('click' , function(){
+          overlay.remove()
+           snowFall()
+          setInterval(() => {
+               snowFall()
+          }, 1000);
+          var tl = anime.timeline({
+               easing: 'easeOutExpo',
+               duration: 2000,
+          });
+          tl
+          .add({
+               targets: heroText[0],
+               top : [50,70, 0],
+               opacity : [0, 1,1, 0],
+          })
+          .add({
+               targets: heroText[1],
+               top : [50,70, 0],
+               opacity :  [0, 1,1, 0],
+          })
+          .add({
+               targets: heroText[2],
+               top : [50,70, 0],
+               opacity :  [0, 1,1, 0],
+          })
+          .add({
+               targets: heroText[3],
+               top : [50,70, 0],
+               opacity :  [0, 1,1, 0],
+          })
+          .add({
+               targets: heroText[4],
+               top : [50,70, 0],
+               opacity :  [0, 1,1, 0],
+          })
+          .add({
+               targets: 'img',
+               opacity :  1,
+          })
      })
-     .add({
-          targets: heroText[1],
-          opacity : 1
-     })
+
 
 })
